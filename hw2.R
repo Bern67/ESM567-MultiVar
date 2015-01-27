ce25905fc7545f8d089d019ad4fb0a6b4b612998
Bern67 <h2o.bern@gmail.com>
2015-01-26 19:35:35 -0800

    minor changes

--- hw2.R
+++ hw2.R
@@ -4,22 +4,25 @@ dta <- read.csv("evenv.csv")
 dta <- na.omit(dta)
 library(dplyr)
 dta <- select(dta,-Site)
-dta <- filter(dta, CHLAsed>0) # need to remove zeros if log transforming data
+detach()
 
 boxplot(dta)
 boxplot(scale(dta))
-boxplot(log(scale(dta)))
 
-#run cor.matrix function
+#source cor.matrix function
+source(cor.matrix.r)
 cor.matrix(dta)
-var(scale(dta)) #calculate variance and covariance matrix with the standardized data
 
+round(var(scale(dta)),2) #calculate variance and covariance matrix with the standardized data
 
-require(MASS) #loads teh PCA package
-pca <- princomp(scale(dta[,-c(1,9)])) #creates a PC matrix
-pca$scores # Show PC matrix
+require(MASS) #loads the PCA package
+pca <- princomp(dta, scores=TRUE,cor=TRUE) #creates a PC matrix using the correlation matrix
 summary(pca) #proportion of variance is eigenvalues for each PC
+plot(pca) #Scree plot
 
-broken.stick(13) #After comparing, keep comp 1 & 2
-biplot(pca)
-loadings(pca)
+source(broken.stick.r)
+broken.stick(15) #After comparing, keep comp 1 & 2
+biplot(pca, pc.biplot = FALSE)
+loadings(pca) #Check eigenvectors: How closely a component and variables are related
+pca$scores #Shows the rotated/transformed dataset: PC matrix
+

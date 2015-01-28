0dca75538bfa8637a64bd2aad6d73b61367b4cf7
Bern67 <h2o.bern@gmail.com>
2015-01-27 23:16:48 -0800

    Vegan biplot added

--- hw2.R
+++ hw2.R
@@ -17,25 +17,22 @@ round(var(env),2) #actual variance and covariance (not good, one variable not sa
 round(var(scale(env)),2) #calculate variance and covariance matrix with the standardized data: Z-score from -1 to 1
 
 require(MASS) #loads the PCA package
-pca <- princomp(env, scores=TRUE,cor=TRUE) #creates a PC matrix using the correlation matrix
+pca <- princomp(env, cor=TRUE) #creates a PC matrix using the correlation matrix
 summary(pca) #proportion of variance is eigenvalues for each PC
 
 plot(pca, main="Scree Plot") #Scree plot
 broken.stick(15) #After comparing, keep comp 1 & 2
 
-biplot(pca, main = "Biplot", xlab = "Comp.1 (26.3%)", ylab = "Comp.2 (23.8%)")
+par(mfrow=c(1,2))
+biplot(pca, expand = 1.05,main = "Biplot", xlab = "Comp.1 (26.3%)", ylab = "Comp.2 (23.8%)")
 loadings(pca) #Check eigenvector elements: How closely variables and components are related; Principal component loading (pg 50)
 pc <- round((pca$scores),2) #Shows the rotated/transformed dataset: PC matrix
 pc
 
 
-
-
 #plaing with vegan package
 library(vegan)
-ord <-metaMDS(env)
-plot(ord, disp="sites", type="n")
-ordihull(ord, Management, col="blue")
-ordiellipse(ord, Management, col=3,lwd=2)
-ordispider(ord, Management, col="red", label = TRUE)
-points(ord, disp="sites", pch=21, col="red", bg="yellow", cex=1.3)
+mod <-rda(env, scale = TRUE)
+biplot(mod,scaling = 3)
+biplot(mod, scaling = 3, type = c("text", "points"))
+summary(mod)

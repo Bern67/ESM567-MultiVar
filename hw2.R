fb212547cd65b669088be08929497c968ebb0006
Bern67 <h2o.bern@gmail.com>
2015-01-30 23:10:15 -0800

    Edits and additions for hw2

--- hw2.R
+++ hw2.R
@@ -9,29 +9,28 @@ boxplot(env, main = "Not scaled")
 boxplot(scale(env), main="Scaled with Z-score")
 
 #source cor.matrix function
-cor.matrix(env)
+var(scale(env)) #calculate correlatin matrix with the standardized data: 
+#Z-score from -1 to 1 (PCC)
+cor.matrix(scale(env))
 
-round(var(scale(env)),2) #calculate variance and covariance matrix with the standardized data: Z-score from -1 to 1
+round(var(env),2) #covariance matrix
+diag(round(var(env),2)) #show variance only from covariance/var matrix (along diagonal)
 
 require(MASS) #loads the PCA package
 pca <- princomp(env, cor=TRUE) #creates a PC matrix using the correlation matrix
+biplot(pca, expand = 1.05,main = "Biplot", xlab = "Comp.1 (26.3%)", ylab = "Comp.2 (23.8%)")
+#Scale for sites on top, scale for variables (vectors) along bottom
 summary(pca) #proportion of variance is eigenvalues for each PC
-princomp(env) #Standard deviations
 
 plot(pca, main="Scree Plot") #Scree plot
 broken.stick(15) #After comparing, keep comp 1 & 2
 
-par(mfrow=c(1,2))
-biplot(pca, expand = 1.05,main = "Biplot", xlab = "Comp.1 (26.3%)", ylab = "Comp.2 (23.8%)")
-loadings(pca) #Check eigenvector elements: How closely variables and components are related; Principal component loading (pg 50)
-round((pca$scores),2) #Shows the rotated/transformed dataset: PC matrix
+round(loadings(pca),2) #Check eigenvectors: How much variability each variable contributes to components; 
+# Principal component loading (pg 50).  The further from zero, the greater the contribution.
+round(loadings(pca)[,c(1:2)],2) #Loading for PC1 & 2 only
 
+round((pca$scores),2) #PC matrix showing site scores for all PCs. 
 
-#plaing with vegan package  
-library(vegan)
-mod <-rda(env, scale = TRUE)
-biplot(mod, scaling = 3)
-biplot(mod, scaling = 3, type = c("text", "points"))
-summary(mod)
-
+require(BiplotGUI)
+Biplots(env)
 

e5cba092afc56736f0136d3f175c9a8046c0e35e
Bern67 <h2o.bern@gmail.com>
2015-02-11 20:23:17 -0800

    Added outline for homework

--- hw3pca.R
+++ hw3pca.R
@@ -28,32 +28,36 @@ pca <- princomp(wtr.lg, cor=TRUE) #creates a PC matrix using the correlation mat
 biplot(pca, main = "Biplot", xlab = "Comp.1 (48.8%)", ylab = "Comp.2 (14.8%)")
 #Scale for sites(PC matrix-pca$scores) on top, scale for variables (vectors-loadings) along bottom
 summary(pca) #proportion of variance is eigenvalues for each PC
+# How well these PCs represent the original data in term of variance and why? (eigenvalues)
 
+broken.stick(18) #After comparing, 1 & 2 above broken.strick
 plot(pca, main="Scree Plot") #Scree plot
-str(wtr)
-broken.stick(18) #After comparing, 1 & 2 above broken.strick 
 
 round(loadings(pca),2) #Check eigenvectors: All very similar, look at groups instead
+#What does each PC mean in terms of original variables? (eigenvectors)
+
 pca$scores
 
 ##calculate Euclidean distance among sites
-wtr.d<-round(var(scale(wtr,scale=F)),0)  #calculate variance-covariance matrix and save it to 'wtr.d'
+wtr.d<-round(var(scale(wtr.lg,scale=F)),0)  #calculate variance-covariance matrix and save it to 'wtr.d'
 e.1<-eigen(wtr.d) #eigen-analysis
-pc.matrix.1<-(as.matrix(scale(wtr, scale=F)))%*%e.1$vectors #calculate pc.matrix
-euc<-dist(scale(wtr, scale=F))  #calculate Euclidian distance among site for centered original data
+pc.matrix.1<-(as.matrix(scale(wtr.lg, scale=F)))%*%e.1$vectors #calculate pc.matrix
+euc<-dist(scale(wtr.lg, scale=F))  #calculate Euclidian distance among site for centered original data
 round(euc,2)
 euc.1<-dist(pc.matrix.1[,c(1,2)])  #calculate Euclidian distance among sites in PCA space using only first 2 PCs
 round(euc.1,2)
 plot(euc,euc.1,main="Shepard diagram", xlab="Distance in Multidimensional space", ylab="Distance in Reduced space")
-#The plot shows the 
+#The plot shows the distortion that occurs when converting from multidimentional space to two dimentions.
+
+#What are spatial patterns/trends among sites in the reduced PCA space?
+#(bubble plots, use other categorical grouping variables, etc.) 
+
+
+
+#Regress PC 1 or 2 with additional explanatory variables   
 
 
 #-------
-wq <- pnw[c(2:9, 11:20)] #water quality variables
-ws <-pnw[c(21,23:24,26:32)] #watershed variables
 
-#make sure to scale data
-boxplot(scale(log(wq+1))) 
-boxplot(scale(ws))
 
 
--- hw3rda.R
+++ hw3rda.R
@@ -5,7 +5,12 @@ pnw <- na.omit(dta) #remove missing data
 rm(dta)
 str(pnw)
 
-#log transformed, centered to Z score
+#Centered to zero with Z-score
+wq <-scale(pnw[c(2:9, 11:20)]) #water quality variables
+ws <-scale(pnw[c(21,23:24,26:32)])
+boxplot(wq)
+boxplot(ws)
+#log transformed, centered to Z-score
 wq.l <-scale(log(pnw[c(2:9, 11:20)]+1)) #water quality variables
 ws.l <-scale(log(pnw[c(21,23:24,26:32)]+1)) #watershed variables
 boxplot(wq.l)
@@ -14,8 +19,35 @@ boxplot(ws.l)
 
 #RDA
 library(vegan)
+library(MASS)
 mod<-rda(wq.l~ws.l, scale=T)
 mod<-rda(wq.l~.,data=ws.l, scale=T)
 mod
 summary(mod)
 plot(mod)
+
+
+
+#1 RDA Full model
+#How well two matrices are associated? (constrained inertia)
+#Is the RDA model relationship between the two matrices significant?	(anova.cca: global permutation test)
+#Can the RDA model be reduced?(step selection with AIC plus vif)
+                  
+#2 RDA reduced model
+#How well two matrices are associated? (constrained inertia)
+#Is the RDA model relationship between the two matrices significant?	(anova.cca: global permutation test)
+#How many RDA axes are significant? (anova.cca: by “axis”)	
+#How many RDA axes to interpret? (eigenvalues for each axis)
+#What each RDA axis represents? (Biplot scores for constraining variables)
+#What are spatial patterns/trends among sitesin the reduced RDA space? 
+# Compare spatial patterns/trends among sites between PCA and RDA plots 
+# Compare eigenvalues of RDA1 and PC1 
+# Interpret the relationships between the two matrices using key results
+
+#3 Variance partition with partial RDA:
+
+#Can we group Xs into big categories (natural vs. anthropogenic)?
+# Which category of Xs is more important than others (varpart) on Ys?
+# What are their interactive effects on Ys?
+
+
